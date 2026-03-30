9f304ebb7a6110e54eb43e7f6c5c5c940f447e6f
Michael3255 <mclcollege1@gmail.com>
2026-03-30 18:24:14 -0400

    updated App and main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
-import Dashboard from "./pages/Dashboard"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import MovieDetails from "./pages/MovieDetails";
+import AddMovie from "./pages/AddMovie";
 
 function App() {
-
   return (
-    <>
-      return <Dashboard />
-    </>
-  )
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/movie/:id" element={<MovieDetails />} />
+        <Route path="/add" element={<AddMovie />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
