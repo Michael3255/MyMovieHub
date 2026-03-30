c2db695a0b9769d9edca3ffcf11c7043f068cff9
Michael3255 <mclcollege1@gmail.com>
2026-03-30 18:19:32 -0400

    feat: add TMDB search view and auth_api app

--- backend/movies/views.py
+++ backend/movies/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
 
 # Create your views here.
+import os
+import requests # to pull third party apis
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+# I definitely used AI here because I wasn't sure where to start with the tMDB api
+
+@api_view(['GET'])
+def search_movies(request):
+
+    query = request.query_params.get('query', '')
+
+    if not query:
+        return Response({'error': 'Please provide a search query.'})
+
+    api_key = os.getenv('TMDB_API_KEY')
+
+    print('API KEY IS:', api_key)
+
+    url = 'https://api.themoviedb.org/3/search/movie'
+
+    params = {
+        'api_key': api_key,
+        'query': query,
+        'language': 'en-US',
+        'page': 1
+    }
+
+    tmdb_response = requests.get(url, params=params)
+
+    print('TMDB STATUS CODE:', tmdb_response.status_code)
+    print('TMDB RAW RESPONSE:', tmdb_response.json())
+
+    data = tmdb_response.json()
+
+    results = []
+
+    for item in data.get('results', []):
+
+        poster_path = item.get('poster_path')
+
+        if poster_path:
+            poster_url = 'https://image.tmdb.org/t/p/w500' + poster_path
+        else:
+            poster_url = None
+
+        release_date = item.get('release_date', '')
+
+        if release_date:
+            release_year = release_date[:4]
+        else:
+            release_year = None
+
+        movie = {
+            'tmdb_id': item.get('id'),
+            'title': item.get('title'),
+            'release_year': release_year,
+            'description': item.get('overview'),
+            'poster_url': poster_url,
+            'genres': item.get('genre_ids', []),
+        }
+
+        results.append(movie)
+
+    return Response({'results': results})
